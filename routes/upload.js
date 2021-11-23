d6d5cb2ee517fec92cd36a3c034290c757a14d3f
Magnus Parmo <73383707+ParmoDev@users.noreply.github.com>
2021-11-23 23:58:54 +0100

    Add files via upload

---
+++ routes/upload.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+router.post("/", function(req, res) {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  let uploadedFile = req.files.uploadedFile;
+  let uploadPath = __dirname + '/../public/files' + uploadedFile.name;
+
+  uploadedFile.mv(uploadPath, function (err) {
+    if (err) {
+      res.send(err)
+      return res.status(500)
+    }
+
+    res.send("File successfully uploaded with name: " + uploadedFile.name)
+  });
+
+});
+
+module.exports = router;
