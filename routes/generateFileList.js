f88af09fce3c2e8d566aa743e64609f66b301d53
Magnus Parmo <73383707+ParmoDev@users.noreply.github.com>
2021-11-25 09:51:33 +0100

    Add files via upload

---
+++ routes/generateFileList.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+let tracks;
+let trackData;
+
+
+router.get('/', function(req, res) {
+  fs.readdir('public/files', function (err, files) {
+    if (err) {
+      console.log(err)
+      return
+    }
+
+    tracks = []
+
+    files.forEach(function (file) {
+      trackData = {
+        name: file,
+        path: 'assets/Audio/' + file
+      }
+
+      tracks.push(trackData)
+    });
+
+    res.send(tracks)
+    return tracks;
+  })
+});
+
+module.exports = router;
--- routes/upload.js
+++ routes/upload.js
@@ -2,20 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 router.post("/", function(req, res) {
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
-  }
+  
+    console.log(req.files)
+    //console.log(Object.keys(req.files))
 
-  let uploadedFile = req.files.uploadedFile;
-  let uploadPath = __dirname + '/../public/files' + uploadedFile.name;
-
-  uploadedFile.mv(uploadPath, function (err) {
-    if (err) {
-      res.send(err)
-      return res.status(500)
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
     }
 
-    res.send("File successfully uploaded with name: " + uploadedFile.name)
+    let uploadedFile = req.files.uploadedFile;
+    let uploadPath = __dirname + '/../public/files' + uploadedFile.name;
+
+    uploadedFile.mv(uploadPath, function (err) {
+        if (err) {
+            res.send(err)
+            return res.status(500)
+        }
+
+        res.send("File successfully uploaded with name: " + uploadedFile.name)
   });
 
 });
